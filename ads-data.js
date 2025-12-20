e5847d8d7a5219261cbde40e402c2f2d5c26e693
Amit Mund <Amitmund@users.noreply.github.com>
2025-12-20 22:58:32 +0530

    Update ads-data.js

--- ads-data.js
+++ ads-data.js
@@ -16,6 +16,12 @@ window.ADS_CONFIG = {
       height: 150
     },
     {
+      image: "https://images.pexels.com/photos/18105/pexels-photo.jpg",
+      text: "Adding the new ads on the offer of laptop sell.",
+      width: 300,
+      height: 180
+    },
+    {
       image: "https://images.pexels.com/photos/267371/pexels-photo-267371.jpeg",
       text: "Flat 50% Discount",
       width: 200,
