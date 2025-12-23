f76ea8f862952d10947c2ee52cb4ec17d3847d66
Amit Mund <Amitmund@users.noreply.github.com>
2025-12-23 19:03:18 +0530

    Create ads-domain-map.js

---
+++ ads-domain-map.js
@@ -0,0 +1,13 @@
+window.ADS_DOMAIN_MAP = {
+  "sretoolkit.com": {
+    enabled: true,
+    adSet: "tech"
+  },
+  "www.sretoolkit.com": {
+    enabled: true,
+    adSet: "tech"
+  },
+  "example.com": {
+    enabled: false
+  }
+};
